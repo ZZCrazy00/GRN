f4f5e2783c52e7133eea1f4229a4bc4c4fe40705
ZZCrazy <104438336+ZZCrazy00@users.noreply.github.com>
2025-12-11 13:17:38 +0800

    Add files via upload

--- get_data.py
+++ get_data.py
@@ -5,15 +5,14 @@ from sklearn.model_selection import train_test_split
 import torch
 from torch_geometric.data import Data
 
-
-# E9.5_E1S1.MOSTA
-# E9.5_E2S1.MOSTA
 # GSM4745615_DBiT-seq_processed
+# MOB_processed     # seqFISH+
 # GSM5173925_OB1-Slide01_slide-seqv2_processed
-# MOB_processed
 # V1_Mouse_Kidney
 # Visium_19_CK297
-data_name = 'Visium_19_CK297'
+# E9.5_E1S1.MOSTA
+# seqFISH(embryo1)
+data_name = 'GSM5173925_OB1-Slide01_slide-seqv2_processed'
 
 # 读取特征
 gene_feature = pd.read_csv(f"dataset/{data_name}/gene_expression.csv", index_col=0)  # target 的特征
@@ -28,7 +27,7 @@ receptor_gene_list = cell_receptor.index.tolist()  # 718
 
 # 读取 GRN 数据并提取 TF 和 target 基因列表
 # human mouse
-data_type = 'human'
+data_type = 'mouse'
 GRN_database = pd.read_csv("resource/{}/TF_TG{}db.csv".format(data_type, data_type),
                            index_col=0).drop_duplicates()  # 9572491 280835 230571
 GRNs = []
@@ -63,10 +62,44 @@ test_pos['label'] = 1
 neg_data = total_data.merge(train_pos, on=['receptor', 'target'], how='outer', indicator=True)
 neg_data = neg_data[neg_data['_merge'] == 'left_only'].drop('_merge', axis=1)
 
+
+# 排除在相同细胞内有表达的基因对
+def filter_coexpressed_pairs(neg_data, cell_receptor, gene_feature):
+    """
+    过滤掉在相同细胞内有表达的受体-靶基因对
+    """
+    filtered_neg_data = []
+
+    for idx, row in tqdm.tqdm(neg_data.iterrows(), total=len(neg_data), desc="Filtering co-expressed pairs"):
+        receptor = row['receptor']
+        target = row['target']
+
+        # 检查受体和靶基因是否在同一个细胞中同时表达
+        if receptor in cell_receptor.index and target in gene_feature.index:
+            receptor_expression = cell_receptor.loc[receptor] > 0
+            target_expression = gene_feature.loc[target] > 0
+
+            # 如果存在至少一个细胞中两者都表达，则排除该对
+            coexpressed_cells = receptor_expression & target_expression
+            if not coexpressed_cells.any():
+                filtered_neg_data.append(row)
+        else:
+            # 如果基因不在表达矩阵中，保留该对
+            filtered_neg_data.append(row)
+
+    return pd.DataFrame(filtered_neg_data)
+
+
+# 应用过滤函数
+print("Filtering co-expressed pairs from negative samples...")
+neg_data_filtered = filter_coexpressed_pairs(neg_data, cell_receptor, gene_feature)
+print(f"Original negative samples: {len(neg_data)}")
+print(f"After filtering co-expressed pairs: {len(neg_data_filtered)}")
+
 # 从负样本中随机选择与正样本数量相等的样本
-train_neg = neg_data.sample(n=len(train_pos), random_state=42)
-val_neg = neg_data.sample(n=len(val_pos), random_state=42)
-test_neg = neg_data.sample(n=len(test_pos), random_state=42)
+train_neg = neg_data_filtered.sample(n=len(train_pos), random_state=43)
+val_neg = neg_data_filtered.sample(n=len(val_pos), random_state=43)
+test_neg = neg_data_filtered.sample(n=len(test_pos), random_state=43)
 train_neg['label'] = 0
 val_neg['label'] = 0
 test_neg['label'] = 0
@@ -103,11 +136,11 @@ def get_pyg_data(node_feature, cell_receptor, gene_feature, data, edge_index=Non
 
 
 train_data = get_pyg_data(node_features, cell_receptor, gene_feature, train_data)
-torch.save(train_data, 'dataset/{}/train_data.pt'.format(data_name))
+torch.save(train_data, 'dataset/{}/train_data_new.pt'.format(data_name))
 val_data = get_pyg_data(node_features, cell_receptor, gene_feature, val_data, train_data.edge_index)
-torch.save(val_data, 'dataset/{}/val_data.pt'.format(data_name))
+torch.save(val_data, 'dataset/{}/val_data_new.pt'.format(data_name))
 test_data = get_pyg_data(node_features, cell_receptor, gene_feature, test_data, train_data.edge_index)
-torch.save(test_data, 'dataset/{}/test_data.pt'.format(data_name))
+torch.save(test_data, 'dataset/{}/test_data_new.pt'.format(data_name))
 
 print(train_data)
 print(val_data)
